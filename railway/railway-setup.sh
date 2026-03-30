c73e0234659c98ce3726bb33e64dd0b6f8cdd5c3
linhtinhforfree <linhtinhforfree@gmail.com>
2026-03-30 10:57:02 +0700

    Enhance railway setup script to install additional apps
    Added installation of payments and hrms apps during setup.

--- railway/railway-setup.sh
+++ railway/railway-setup.sh
@@ -11,6 +11,13 @@ echo "{}" > /home/frappe/bench/sites/common_site_config.json
 
 echo "-> Create new site with ERPNext"
 bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${RFP_DB_ROOT_PASSWORD} --install-app erpnext
+
+echo "-> Install payments app on site"
+bench --site ${RFP_DOMAIN_NAME} install-app payments
+
+echo "-> Install hrms app on site"
+bench --site ${RFP_DOMAIN_NAME} install-app hrms
+
 bench use ${RFP_DOMAIN_NAME}
 
 echo "-> Enable scheduler"
